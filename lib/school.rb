82091916dc00b46191a287c1d8c17594f8be2d53
Terrence Bell <terrencekbell@gmail.com>
2020-03-12 16:51:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
        @roster
     end 
     def add_student(name, grade)
-      @roster[grade] = []
-      @roster[grade] << name 
+      @roster[grade] = [name]
+      @rost
   end
 end
